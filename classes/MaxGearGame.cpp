8b36525c2f5f61a18b2eab09f66887cc391cfaa7
jrrlucenafilho <jrrlucenafilho@gmail.com>
2024-11-22 07:49:15 -0300

    Removed face detection code

--- classes/MaxGearGame.cpp
+++ classes/MaxGearGame.cpp
@@ -29,8 +29,6 @@ Menu MaxGearGame::getMenu() { return menu; }
 void MaxGearGame::setTrackOption(int option) { trackOption = option; }
 
 bool MaxGearGame::OnUserCreate() {
-  faceDetectionBegin();
-
   waveEngine.InitialiseAudio();  // Start respective track's audio
   setTrackAudio();
   waveEngine.PlayWaveform(&trackAudio);
@@ -46,22 +44,9 @@ bool MaxGearGame::OnUserUpdate(float fElapsedTime) {
   // Force car to direction 0 if not going to sides
   car.setnCarDirection(CAR_STILL);
 
-  if (capture.isOpened()) {
-    capture >> frame;
-    if (frame.empty()) {
-      return false;
-    }
-
-    detectAndDraw();
-
-    // Main quitting logic
-    char c = (char)pollKey();  // Needed to open camera window
-    if ((c == 27) || (c == 'q') || (c == 'Q')) {
-      return false;
-    }
-    if (GetKey(Key::Q).bPressed) {
-      return false;
-    }
+  // Main quitting logic
+  if (GetKey(Key::Q).bPressed) {
+    return false;
   }
 
   if ((GetKey(Key::UP).bHeld) || (GetKey(Key::SPACE).bHeld) ||
@@ -73,13 +58,13 @@ bool MaxGearGame::OnUserUpdate(float fElapsedTime) {
 
   // Car Curvature is accumulated left/right input (but inv proportional to
   // speed) To make it harder to turn at high speed
-  if ((GetKey(Key::LEFT).bHeld) || (ncvDirection == CV_LEFT)) {
+  if ((GetKey(Key::LEFT).bHeld)) {
     car.deductFromfCarCurvature(0.7f * fElapsedTime *
                                 (1.0f - car.getfSpeed() / 2.0f));
     car.setnCarDirection(CAR_LEFT);
   }
 
-  if ((GetKey(Key::RIGHT).bHeld) || (ncvDirection == CV_RIGHT)) {
+  if ((GetKey(Key::RIGHT).bHeld)) {
     car.addTofCarCurvature(0.7f * fElapsedTime *
                            (1.0f - car.getfSpeed() / 2.0f));
     car.setnCarDirection(CAR_RIGHT);
@@ -236,9 +221,6 @@ bool MaxGearGame::OnUserUpdate(float fElapsedTime) {
 }
 
 bool MaxGearGame::OnUserDestroy() {
-  destroyWindow("Capture Camera");
-  capture.release();
-
   // Sort and save record to file
   listLapTimes.sort();
   for (float lapTime : listLapTimes) {
@@ -268,30 +250,6 @@ void MaxGearGame::setTrackAudio() {
       trackAudio.LoadAudioWaveform("assets/sounds/easterEgg_ost.wav");
       break;
   }
-}
-
-void MaxGearGame::faceDetectionBegin() {
-  cout << "Iniciando captura de vídeo ..." << '\n';
-
-  // cascadeName = "assets/haarcascade_frontalface_default.xml";
-  cascadeName = "assets/haarcascade_frontalface_alt2.xml";
-  scale = 1;
-  if (scale < 1) {
-    scale = 1;
-  }
-  tryflip = true;
-
-  if (!cascade.load(cascadeName)) {
-    cerr << "ERROR: Could not load classifier cascade" << '\n';
-    return;
-  }
-
-  if (!capture.open(0)) {
-    cout << "Capture from camera #0 didn't work" << '\n';
-    return;
-  }
-
-  return;
 }
 
 string MaxGearGame::getRecordFromFile() {
@@ -329,88 +287,4 @@ void MaxGearGame::writeRecordToFile() {
 
   fp << "Recorde: " << hud.displayTime(timeRecord) << '\n';
   fp.close();
-}
-
-/**
- * @brief Draws a transparent image over a frame Mat.
- *
- * @param frame the frame where the transparent image will be drawn
- * @param transp the Mat image with transparency, read from a PNG image, with
- * the IMREAD_UNCHANGED flag
- * @param xPos x position of the frame image where the image will start.
- * @param yPos y position of the frame image where the image will start.
- */
-void MaxGearGame::drawTransparency(Mat frame, Mat transp, int xPos, int yPos) {
-  Mat mask;
-  vector<Mat> layers;
-
-  split(transp, layers);  // Seperate channels
-  Mat rgb[3] = {layers[0], layers[1], layers[2]};
-  mask = layers[3];  // PNG's alpha channel used as mask
-  merge(
-      rgb, 3,
-      transp);  // Put together the RGB channels, now transp insn't transparent
-  transp.copyTo(frame.rowRange(yPos, yPos + transp.rows)
-                    .colRange(xPos, xPos + transp.cols),
-                mask);
-}
-
-void MaxGearGame::drawTransRect(Mat frame, Scalar color, double alpha,
-                                Rect region) {
-  Mat roi = frame(region);
-  Mat rectImg(roi.size(), CV_8UC3, color);
-  addWeighted(rectImg, alpha, roi, 1.0 - alpha, 0, roi);
-}
-
-void MaxGearGame::detectAndDraw() {
-  // double t = 0;
-  vector<Rect> faces;
-  Mat gray, smallImg;
-  Scalar color = Scalar(255, 0, 0);
-
-  if (tryflip) {
-    flip(frame, frame, 1);
-  }
-
-  cvtColor(frame, gray, COLOR_BGR2GRAY);
-  double fx = 1 / scale;
-
-  resize(gray, smallImg, Size(), fx, fx, INTER_LINEAR_EXACT);
-  equalizeHist(smallImg, smallImg);
-
-  cascade.detectMultiScale(smallImg, faces, 1.3, 2, 0 | CASCADE_SCALE_IMAGE,
-                           Size(40, 40));
-
-  // Runs through found faces
-  for (size_t i = 0; i < faces.size(); i++) {
-    Rect r = faces[i];
-    rectangle(frame, Point(cvRound(r.x * scale), cvRound(r.y * scale)),
-              Point(cvRound((r.x + r.width - 1) * scale),
-                    cvRound((r.y + r.height - 1) * scale)),
-              color, 3);
-
-    // Detects rectangles' positions and evaluate if they're on the left/right
-    // side, assigning it to the cv car control variable
-    if (cvRound(r.x * scale) > 130 && cvRound(r.x * scale) < 270) {
-      ncvDirection = CV_STILL;
-    } else if (cvRound(r.x * scale) < 120) {
-      ncvDirection = CV_LEFT;
-    } else if (cvRound(r.x * scale) > 200) {
-      ncvDirection = CV_RIGHT;
-    }
-  }
-
-  // Draw arrows on window
-  Mat leftArrowOverlay = cv::imread("assets/arrow_left.png", IMREAD_UNCHANGED);
-  drawTransparency(frame, leftArrowOverlay, 10, 20);
-
-  Mat rightArrowOverlay =
-      cv::imread("assets/arrow_right.png", IMREAD_UNCHANGED);
-  drawTransparency(frame, rightArrowOverlay, 530, 20);
-
-  // Transparent squares for signaling position to control car
-  drawTransRect(frame, Scalar(0, 255, 0), 0.3, Rect(0, 0, 130, 480));
-  drawTransRect(frame, Scalar(255, 0, 0), 0.3, Rect(510, 0, 130, 480));
-
-  cv::imshow("Capture Camera", frame);
 }
--- headers/MaxGearGame.h
+++ headers/MaxGearGame.h
@@ -3,74 +3,63 @@
 
 #pragma once
 
-#include <fstream>
 #include <algorithm>
-#include "opencv2/objdetect.hpp"
-#include "opencv2/highgui.hpp"
-#include "opencv2/imgproc.hpp"
-#include "opencv2/videoio.hpp"
+#include <fstream>
+
+#include "Car.h"
+#include "HUD.h"
+#include "Menu.h"
+#include "Tracks.h"
 #include "olcPixelGameEngine.h"
 #include "olcSoundWaveEngine.h"
-#include "HUD.h"
-#include "Car.h"
-#include "Tracks.h"
-#include "Menu.h"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/objdetect.hpp"
+#include "opencv2/videoio.hpp"
 
-enum cvDirections {CV_STILL = 0, CV_LEFT = -1, CV_RIGHT = 1};
+enum cvDirections { CV_STILL = 0, CV_LEFT = -1, CV_RIGHT = 1 };
 
-class MaxGearGame : public olc::PixelGameEngine
-{
-    public:
-        MaxGearGame();
-        virtual ~MaxGearGame();
+class MaxGearGame : public olc::PixelGameEngine {
+ public:
+  MaxGearGame();
+  virtual ~MaxGearGame();
 
-        float getfDistance();
-        float getfAccTrackCurvature();
-        float getfTargetCurvature();
-        float getfCurrentLapTime();
-        std::list<float> getListLapTimes();
-        Menu getMenu();
+  float getfDistance();
+  float getfAccTrackCurvature();
+  float getfTargetCurvature();
+  float getfCurrentLapTime();
+  std::list<float> getListLapTimes();
+  Menu getMenu();
 
-        void setTrackOption(int option);    //CV related
-        void faceDetectionBegin();
+  void setTrackOption(int option);  // CV related
 
-        std::string getRecordFromFile();    //Printing and getting record to file
-        void writeRecordToFile();
+  std::string getRecordFromFile();  // Printing and getting record to file
+  void writeRecordToFile();
 
-    protected:
-        virtual bool OnUserCreate();    //Called by the engine
-        virtual bool OnUserUpdate(float fElapsedTime);
-        virtual bool OnUserDestroy();
+ protected:
+  virtual bool OnUserCreate();  // Called by the engine
+  virtual bool OnUserUpdate(float fElapsedTime);
+  virtual bool OnUserDestroy();
 
-    private:
-        void setTrackAudio();
-        void detectAndDraw();
-        void drawTransparency(cv::Mat frame, cv::Mat transp, int xPos, int yPos);
-        void drawTransRect(cv::Mat frame, cv::Scalar color, double alpha, cv::Rect region);
+ private:
+  void setTrackAudio();
 
-        Menu menu;
-        Tracks tracks;
-        HUD hud;
-        Car car;
+  Menu menu;
+  Tracks tracks;
+  HUD hud;
+  Car car;
 
-        olc::sound::WaveEngine waveEngine;  //Sound stuff
-        olc::sound::Wave trackAudio;
+  olc::sound::WaveEngine waveEngine;  // Sound stuff
+  olc::sound::Wave trackAudio;
 
-        int trackOption;    //Which track to build, for colors, sound, etc.
-        float fDistance;    //Distance car has travelled around track
-        float fAccTrackCurvature;   //Acc of track curvature
-        float fTargetCurvature; //Curvature that's being added/subtracted over ElapsedTime
-        float fCurrentLapTime;  //Current lap time
-        std::list<float> listLapTimes;	//5 last lap times
-        float timeRecord;
-
-        std::string cascadeName;    //OpenCV related
-        cv::VideoCapture capture;
-        cv::Mat frame;
-        bool tryflip;
-        cv::CascadeClassifier cascade;
-        double scale;
-        int ncvDirection;
+  int trackOption;           // Which track to build, for colors, sound, etc.
+  float fDistance;           // Distance car has travelled around track
+  float fAccTrackCurvature;  // Acc of track curvature
+  float fTargetCurvature;    // Curvature that's being added/subtracted over
+                             // ElapsedTime
+  float fCurrentLapTime;     // Current lap time
+  std::list<float> listLapTimes;  // 5 last lap times
+  float timeRecord;
 };
 
 #endif
