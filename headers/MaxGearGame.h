cbfe872fba7b58dd5a25f5a6d6e31c289b0054c4
jrrlucenafilho <jrrlucenafilho@gmail.com>
2024-12-02 14:40:14 -0300

    fix: readded cv functions for main branch

--- headers/MaxGearGame.h
+++ headers/MaxGearGame.h
@@ -12,6 +12,10 @@
 #include "Tracks.h"
 #include "olcPixelGameEngine.h"
 #include "olcSoundWaveEngine.h"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/objdetect.hpp"
+#include "opencv2/videoio.hpp"
 
 enum cvDirections { CV_STILL = 0, CV_LEFT = -1, CV_RIGHT = 1 };
 
@@ -27,7 +31,8 @@ class MaxGearGame : public olc::PixelGameEngine {
   std::list<float> getListLapTimes();
   Menu getMenu();
 
-  void setTrackOption(int option);
+  void setTrackOption(int option);  // CV related
+  void faceDetectionBegin();
 
   std::string getRecordFromFile();  // Printing and getting record to file
   void writeRecordToFile();
@@ -39,6 +44,10 @@ class MaxGearGame : public olc::PixelGameEngine {
 
  private:
   void setTrackAudio();
+  void detectAndDraw();
+  void drawTransparency(cv::Mat frame, cv::Mat transp, int xPos, int yPos);
+  void drawTransRect(cv::Mat frame, cv::Scalar color, double alpha,
+                     cv::Rect region);
 
   Menu menu;
   Tracks tracks;
@@ -52,10 +61,18 @@ class MaxGearGame : public olc::PixelGameEngine {
   float fDistance;           // Distance car has travelled around track
   float fAccTrackCurvature;  // Acc of track curvature
   float fTargetCurvature;    // Curvature that's being added/subtracted over
-                             // ElapsedTime
-  float fCurrentLapTime;     // Current lap time
+                           // ElapsedTime
+  float fCurrentLapTime;          // Current lap time
   std::list<float> listLapTimes;  // 5 last lap times
   float timeRecord;
+
+  std::string cascadeName;  // OpenCV related
+  cv::VideoCapture capture;
+  cv::Mat frame;
+  bool tryflip;
+  cv::CascadeClassifier cascade;
+  double scale;
+  int ncvDirection;
 };
 
 #endif
